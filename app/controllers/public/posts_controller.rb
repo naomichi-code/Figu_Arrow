b59a8c810450ba5826bd39e8ab985d80c5722dc1
vagrant <vagrant@localhost.localdomain>
2020-08-19 13:09:27 +0000

    [add]新規投稿画面

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,41 @@
 class Public::PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only:[:show, :edit, :update, :destroy]
+  before_action :screen_user, only: [:edit, :update]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.new(post_params)
+    if post.save
+      redirect_to post_path(post)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  #投稿をセット
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  #カレントユーザー以外がリダイレクト
+  def screen_user
+    unless params[:id].to_i == current_user.id
+        redirect_to user_path(current_user)
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(
+        :title,
+        :body,
+        :requirement,
+        :status,
+        tags_attributes: [:brand_tag, :title_tag, :chara_tag, :_destroy, :id]
+    )
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,18 @@
 class Post < ApplicationRecord
-has_many :arrows
-has_many :post_comments
-has_many :tags
-has_many :item_photos
-belongs_to :user
+  has_many :arrows, dependent: :destroy
+  has_many :post_comments,dependent: :destroy
+  has_many :tags, dependent: :destroy
+  accepts_nested_attributes_for :tags, allow_destroy: true
+  has_many :item_photos, dependent: :destroy
+  accepts_nested_attributes_for :item_photos, allow_destroy: true
+  belongs_to :user
+
+  validates :title, presence: true,length: {maximum: 20, minimum: 1}
+  validates :body, presence: true, length: {maximum: 300}
+  validates :requirement, length: {maximum: 300}
+  #validates :item_photo, plasence:true
+
+
+  enum status: {"設定無し" => 0, "譲渡可" => 1, "交換可" => 2}
+
 end
