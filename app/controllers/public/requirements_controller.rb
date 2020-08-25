1e86d78ff69c66a4589bc6b8e82b88205f2b0b22
vagrant <vagrant@localhost.localdomain>
2020-08-25 09:10:01 +0000

    [add]ジャンル一覽と譲渡一覽機能

--- app/controllers/public/requirements_controller.rb
+++ app/controllers/public/requirements_controller.rb
@@ -1,2 +1,6 @@
 class Public::RequirementsController < ApplicationController
+  def index
+    @gives = Post.where(status: 1)
+    @exchanges = Post.where(status: 2)
+  end
 end
--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -1,8 +1,16 @@
 class Public::TagsController < ApplicationController
   def index
-    @tags = Tag.all
+    @brand = Tag.group(:brand_tag)
+    @title = Tag.group(:title_tag)
+    @chara = Tag.group(:chara_tag)
   end
   def show
-    @tags = Tag.find(params[:id])
+    tag = Tag.find(params[:id])
+    @colum = params[:tag]
+
+    @brand = Tag.where(brand_tag: tag.brand_tag)
+    @title = Tag.where(title_tag: tag.title_tag)
+    @chara = Tag.where(chara_tag: tag.chara_tag)
   end
+
 end
